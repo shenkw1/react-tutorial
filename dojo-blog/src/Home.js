0f0fa42b40dfe84120e8d3b3ff236d1feb0013d1
kshen47 <katherineshen@gatech.edu>
2022-05-17 12:23:03 -0400

    :sparkles: loading msg

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,22 +3,24 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
-
-    const [name, setName] = useState('mario');
+    const [isLoading, setIsLoading] = useState(true);
     
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
             .then(res => {
-                return res.json()
+                return res.json();
             })
             .then((data) => {
-                console.log(data)
-                setBlogs(data)
+                setBlogs(data);
+                setIsLoading(false);
             })
+        }, 1000)
     }, []);
 
     return (
         <div className="home">
+            {isLoading && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
