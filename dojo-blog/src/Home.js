a06e69bbbf663e5e0188a7eb0677c35013b2ad79
kshen47 <katherineshen@gatech.edu>
2022-05-17 14:10:06 -0400

    :sparkles: error handling

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -4,22 +4,33 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
     
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then((data) => {
-                setBlogs(data);
-                setIsLoading(false);
-            })
+                .then(r => {
+                    console.log(r)
+                    if(!r.ok) {
+                        throw Error('could not fetch data for specific resource');
+                    }
+                    return r.json();
+                })
+                .then((data) => {
+                    setBlogs(data);
+                    setIsLoading(false);
+                    setError(null);
+                })
+                .catch(e => {
+                    setIsLoading(false);
+                    setError(e.message);
+                })
         }, 1000)
     }, []);
 
     return (
         <div className="home">
+            {e && <div>{ e }</div>}
             {isLoading && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
