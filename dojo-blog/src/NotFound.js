570f42e394d0987c644be91053c508bacefa37de
kshen47 <katherineshen@gatech.edu>
2022-05-18 11:10:19 -0400

    :tada: finish react course

---
+++ dojo-blog/src/NotFound.js
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <h2>Sorry!</h2>
+            <p>Page not found</p>
+            <Link to="/">Back to homepage...</Link>
+        </div>
+    );
+}
+ 
+export default NotFound;
